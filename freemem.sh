6a391aafbb658f75caac2553764a75702036f66a
ernie <34834531+ernisn@users.noreply.github.com>
2019-01-08 13:41:37 +0800

    添加传参执行方式，优化显示内容

--- freemem.sh
+++ freemem.sh
@@ -46,35 +46,57 @@ else
     [ ${virtua} == "openvz" ] && { echo -e "本机虚拟化架构为${red} OpenVZ ${none}，本脚本不支持${red} OpenVZ ${none}。"; exit 1; }
 fi
 
-# 执行内存释放
-echo "--------------------"
-echo "当前内存使用情况:"
-free -m
-echo "--------------------"
-echo "选择内存释放方式:"
-echo -e "${green}1. ${none}仅释放 pagecache"
-echo -e "${green}2. ${none}释放 dentries + inodes"
-echo -e "${green}3. ${none}释放 pagecache + dentries + inodes"
-echo -e "按${green} Ctrl + C ${none}可取消释放内存"
-echo "--------------------"
-read -p "输入数值进行选择（默认：1）: " selection
+# 传参执行内存释放
+if  [ "$1" == "-p" ] ; then
+    sync
+    echo 1 > /proc/sys/vm/drop_caches && exit 1
+elif [ "$1" == "-di" ] ; then
+    sync
+    echo 2 > /proc/sys/vm/drop_caches && exit 1
+elif [ "$1" == "-all" ] ; then
+    sync
+    echo 3 > /proc/sys/vm/drop_caches && exit 1
+elif [ "$#" != "0" ] ; then
+    echo "————使用说明———————————————————————————————————————————————————————————————————"
+    echo -e "  · bash freemem.sh ${green}-p ${none}仅释放 pagecache"
+	echo -e "  · bash freemem.sh ${green}-di ${none}释放 dentries + inodes"
+	echo -e "  · bash freemem.sh ${green}-all ${none}释放 pagecache + dentries + inodes"
+	echo "———————————————————————————————————————————————————————————————————————————————"
+	exit 1
+fi
+
+# 清理前内存
+echo "————当前内存使用情况———————————————————————————————————————————————————————————"
+free -h
+echo "———————————————————————————————————————————————————————————————————————————————"
+
+# 未传参执行内存释放
+echo "请选择内存释放方式:"
+echo -e "  ${green}1 ${none}仅释放 pagecache"
+echo -e "  ${green}2 ${none}释放 dentries + inodes"
+echo -e "  ${green}3 ${none}释放 pagecache + dentries + inodes"
+echo -e "  * 按${green} Ctrl + C ${none}可取消释放内存"
+echo "———————————————————————————————————————————————————————————————————————————————"
+read -p "输入数值进行选择 (默认:1): " selection
 if  [ ! -n "$selection" ] ; then
     echo "未进行选择，默认仅释放 pagecache 。"
 else
     while [ -n "$selection" ] && [ "$selection" != 1 ] && [ "$selection" != 2 ] && [ "$selection" != 3 ]
         do
-	    echo -e "${red}输入错误${none}，你输的真的是${green} 1 ${none}或${green} 2 ${none}或${green} 3 ${none}吗？"
-        read -p "请重新输入数值进行选择（默认：1）: " selection
+	    echo -e "${red}输入错误${none}，你输入的真的是${green} 1 ${none}或${green} 2 ${none}或${green} 3 ${none}吗？"
+        read -p "请重新输入数值进行选择 (默认:1): " selection
 		[ ! -n "$selection" ] && echo "未进行选择，默认仅释放 pagecache 。" 
     done
 fi
-echo "同步数据中..."
+echo "  · 同步数据中..."
 sync
-echo "完成同步，开始释放内存..."
-[ ! -n "$selection" ] || [ "$selection" == 1 ] && echo "释放 pagecache 中..." && echo 1 > /proc/sys/vm/drop_caches
-[ "$selection" == 2 ] && echo "释放 dentries + inodes 中..." && echo 2 > /proc/sys/vm/drop_caches
-[ "$selection" == 3 ] && echo "释放 pagecache + dentries + inodes 中..." && echo 3 > /proc/sys/vm/drop_caches
-echo "--------------------"
-echo "内存释放完成，清理后内存使用情况:"
-free -m
-echo "--------------------"
+echo "  · 完成同步，开始释放内存..."
+[ ! -n "$selection" ] || [ "$selection" == 1 ] && echo "  · 释放 pagecache 中..." && echo 1 > /proc/sys/vm/drop_caches
+[ "$selection" == 2 ] && echo "  · 释放 dentries + inodes 中..." && echo 2 > /proc/sys/vm/drop_caches
+[ "$selection" == 3 ] && echo "  · 释放 pagecache + dentries + inodes 中..." && echo 3 > /proc/sys/vm/drop_caches
+echo "  · 内存释放完成。"
+
+# 清理后内存
+echo "————当前内存使用情况———————————————————————————————————————————————————————————"
+free -h
+echo "———————————————————————————————————————————————————————————————————————————————"
